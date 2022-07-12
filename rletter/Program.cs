c3c560036b233bc889c73440aceccb005295f822
jedrzej.szyszka <jedrzej.szyszka@WRO1-LDL-P90337>
2022-07-12 09:56:35 +0200

    Add project files.

---
+++ rletter/Program.cs
@@ -0,0 +1,7 @@
+﻿string chars = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+Random rand = new Random();
+int letter = rand.Next(0, chars.Length);
+Console.WriteLine(chars[letter]);
+
+string str = Char.ToString(chars[letter]);
+File.WriteAllText(@"C:\Users\jedrzej.szyszka\Desktop\Results\randomletter.txt", str);
